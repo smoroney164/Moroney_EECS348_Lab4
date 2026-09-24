2453ae003f4790013aaa07cd597385b7b9a93a08
Sydney Moroney <smoroney164@ku.edu>
2026-09-24 11:28:51 -0500

    both files working~

--- task2.C
+++ task2.C
@@ -1,35 +1,62 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(){
-    int og_temp;
+    double og_temp;
     printf("Enter the temperature value: ");
-    scanf("%d", &og_temp);
+    scanf("%lf", &og_temp);
 
-    int og_scale;
+    char og_scale;
     printf("Enter the original scale (C, F, or K): ");
-    scanf("%s", og_scale);
+    scanf(" %c", &og_scale);
 
-    int new_scale;
+    char new_scale;
     printf("Enter the scale to convert to (C, F, or K): ");
-    scanf("%s", new_scale);
+    scanf(" %c", &new_scale);
 
     //convert all to celcius
-    if(og_scale=="f" || og_scale=="F"){
-
+    if(toupper(og_scale)=='F'){
+        og_temp = (og_temp-32)/1.8;
     }
-    else if(og_scale=="k" || og_scale=="K"){
-
+    else if(toupper(og_scale)=='K'){
+        og_temp = og_temp-272.15;
     }
     
     //convert to new scale
-    if(new_scale=="f" || new_scale=="F"){
+    double new_temp;
 
+    if(toupper(new_scale)=='F'){
+        new_temp = ((og_temp)*1.8) + 32;
     }
-    else if(new_scale=="c" || new_scale=="C"){
-
+    else if(toupper(new_scale)=='K'){
+        new_temp = og_temp + 272.15;
     }
-    else if(new_scale=="k" || new_scale=="K"){
+    else if(toupper(new_scale)=='C'){
+        new_temp = og_temp;
+    }
 
+    printf("Converted temperature: %.2f %c\n", new_temp, toupper(new_scale));
+    
+    //print category
+    if(og_temp<0){
+        printf("Temperature category: Freezing\n");
+        printf("Weather advisory: Wear a hat and gloves!\n");
+    }
+    else if(og_temp<10){
+        printf("Temperature category: Cold\n");
+        printf("Weather advisory: Dont forget a jacket!\n");
+    }
+    else if(og_temp<25){
+        printf("Temperature category: Comfortable\n");
+        printf("Weather advisory: Go on a walk!\n");
+    }
+    else if(og_temp<35){
+        printf("Temperature category: Hot\n");
+        printf("Weather advisory: Drink lots of water!\n");
+    }
+    else{
+        printf("Temperature category: Extreme heat\n");
+        printf("Weather advisory: Stay inside!\n");
     }
 
 }
