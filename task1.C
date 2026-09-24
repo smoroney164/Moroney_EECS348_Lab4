fdf1fd700c5ec4ba1e23750a62ea5c1c7bc65198
Sydney Moroney <smoroney164@ku.edu>
2026-09-24 12:02:32 -0500

    comments!

--- task1.C
+++ task1.C
@@ -1,27 +1,34 @@
+
+
 #include <stdio.h>
 
 int main(){
     int score;
 
+    //takes in the goal score from user
     printf("Enter the NFL score (Enter 1 to stop): ");
     scanf("%d", &score);
 
+    //while score is negative, keeps asking for valid input
     while (score<0){
         printf("Score invalid! Please try again!: ");
         scanf("%d", &score);
     }
 
+    //block runs as long as score is not 1, program will stop otherwise
     while (score != 1){
 
         printf("Possible combinations of scoring plays if a team’s score is %d:\n", score);
 
+        //goes through possible number of td2, td1, td, fg, and safetys to each reach the goal score
         for(int td2=0; td2 <= score/8; td2++){
             for(int td1=0; td1 <= score/7; td1++){
                 for(int td=0; td <= score/6; td++){
                     for(int fg=0; fg <= score/3; fg++){
                         for(int safety=0; safety <= score/2; safety++){
-                            int total_score = (td2*8)+(td1*7)+(td*6)+(fg*3)+(safety*2);
-                            
+                            int total_score = (td2*8)+(td1*7)+(td*6)+(fg*3)+(safety*2); //calcuates the total score per each combination
+
+                            //will only print combination if it equals the goal score
                             if(total_score == score){
                                 printf("%d TD + 2pt, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", td2, td1, td, fg, safety);
                             }
@@ -31,6 +38,7 @@ int main(){
             }
         }
 
+        //Asks again so that if the user enter 1, it will not run again.
         printf("Enter the NFL score (Enter 1 to stop): ");
         scanf("%d", &score);
 
--- task2.C
+++ task2.C
@@ -2,58 +2,116 @@
 #include <ctype.h>
 
 int main(){
+
+    //takes in double for temperature. keeps asking until valid input
     double og_temp;
     printf("Enter the temperature value: ");
-    scanf("%lf", &og_temp);
 
+    while(scanf("%lf", &og_temp) != 1){ //if block will run if og_temp is not double or int
+        printf("Invalid temperature!\n");
+        printf("Enter the temperature value: ");
+        while (getchar() != '\n')
+        {
+          // discard invalid input
+        }
+    };
+
+    //------------------------------------------------------------------------------
+
+    //takes in character for original scale. keeps asking until valid input
     char og_scale;
     printf("Enter the original scale (C, F, or K): ");
     scanf(" %c", &og_scale);
 
+    while(toupper(og_scale)!='F' && toupper(og_scale)!='K' && toupper(og_scale)!='C'){
+        printf("Invalid scale!\n");
+    
+        while (getchar() != '\n')
+        {
+          // discard invalid input
+        }
+
+        printf("Enter the original scale (C, F, or K): ");
+        scanf(" %c", &og_scale);
+    }
+
+    //------------------------------------------------------------------------------
+
+    //takes in character for new scale. keeps asking until valid input
     char new_scale;
     printf("Enter the scale to convert to (C, F, or K): ");
     scanf(" %c", &new_scale);
 
+    while(toupper(new_scale)!='F' && toupper(new_scale)!='K' && toupper(new_scale)!='C'){
+        printf("Invalid scale!\n");
+        
+        while (getchar() != '\n')
+        {
+           // discard invalid input
+        }
+
+        printf("Enter the scale to convert to (C, F, or K): ");
+        scanf(" %c", &new_scale);
+    }
+
+    //------------------------------------------------------------------------------
+
     //convert all to celcius
+    
+    //FROM FAHRENHEIT
     if(toupper(og_scale)=='F'){
         og_temp = (og_temp-32)/1.8;
     }
+    //FROM KELVIN
     else if(toupper(og_scale)=='K'){
         og_temp = og_temp-272.15;
     }
-    
+
+    //------------------------------------------------------------------------------
+
     //convert to new scale
     double new_temp;
 
+    //TO FAHRENHEIT
     if(toupper(new_scale)=='F'){
         new_temp = ((og_temp)*1.8) + 32;
     }
+    //TO KELVIN
     else if(toupper(new_scale)=='K'){
         new_temp = og_temp + 272.15;
     }
+    //TO CELCIUS
     else if(toupper(new_scale)=='C'){
         new_temp = og_temp;
     }
 
     printf("Converted temperature: %.2f %c\n", new_temp, toupper(new_scale));
+
+    //------------------------------------------------------------------------------
     
-    //print category
+    //print category and weather advisory
+
+    //FREEZING
     if(og_temp<0){
         printf("Temperature category: Freezing\n");
         printf("Weather advisory: Wear a hat and gloves!\n");
     }
+    //COLD
     else if(og_temp<10){
         printf("Temperature category: Cold\n");
         printf("Weather advisory: Dont forget a jacket!\n");
     }
+    //COMFORTABLE
     else if(og_temp<25){
         printf("Temperature category: Comfortable\n");
         printf("Weather advisory: Go on a walk!\n");
     }
+    //HOT
     else if(og_temp<35){
         printf("Temperature category: Hot\n");
         printf("Weather advisory: Drink lots of water!\n");
     }
+    //EXTREME HEAT
     else{
         printf("Temperature category: Extreme heat\n");
         printf("Weather advisory: Stay inside!\n");
